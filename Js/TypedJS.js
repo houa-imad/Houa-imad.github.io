acaec376c95bfdf9ce535b751683503cdb97ec42
houa-imad <78093122+houa-imad@users.noreply.github.com>
2023-04-01 17:23:37 +0200

    typedJS fixed

--- Js/TypedJS.js
+++ Js/TypedJS.js
@@ -1,12 +1,16 @@
 function animationTypedJS() {
 	let typedAnim = new Typed(".a-proposTxt", {
 		strings: [
-			`<p >Je suis actuellement en formation de développeur web/web mobile dispensé par l'<a class="linksTyped" href="https://www.afpa.fr/" target="_blank">AFPA </a>. Durant cette formation, j'ai acquis des compétences en front-end avec des technologies telles que HTML, CSS, JS et VUE.js, et en back-end avec des technologies comme PHP et SYMFONY.<br>Mon objectif est de devenir un développeur web compétent et polyvalent, capable de concevoir des applications web de haute qualité pour répondre aux besoins des utilisateurs et des clients. Si vous êtes à la recherche d'un développeur passionné et motivé pour votre prochain projet, n'hésitez pas à me <a class="linksTyped" href="#contact">contacter</a>.</p>`,
+			`<p >Je suis actuellement en formation de développeur web/web mobile dispensé par l'<a class="linksTyped" href="https://www.afpa.fr/" target="_blank">AFPA </a>. Durant cette formation, j'ai acquis des compétences en front-end avec des technologies telles que HTML, CSS, JS et VUE.js, et en back-end avec des technologies comme PHP et SYMFONY.</p>`,
+			`<p>Mon objectif est de devenir un développeur web compétent et polyvalent, capable de concevoir des applications web de haute qualité pour répondre aux besoins des utilisateurs et des clients. Si vous êtes à la recherche d'un développeur passionné et motivé pour votre prochain projet, n'hésitez pas à me <a class="linksTyped" href="#contact">contacter</a>.</p>`,
 		],
 		startDelay: 2500,
 		cursorChar: "",
 		typeSpeed: 25,
-		loop: false,
+		backSpeed: 80,
+		loop: true,
+		backDelay: 10000,
+		fadeOut: true,
 	});
 
 	console.log(typedAnim);
