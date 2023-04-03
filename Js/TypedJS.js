3e82846ad5206e045f22650bf9af53010f1e7617
houa-imad <78093122+houa-imad@users.noreply.github.com>
2023-04-04 00:31:07 +0200

    maj 04/04/23

--- Js/TypedJS.js
+++ Js/TypedJS.js
@@ -1,5 +1,14 @@
+// Ajout des boutons pour naviguer entre les différents éléments du typedJS
+const pauseBtn = document
+	.querySelector(".pauseBtn")
+	.addEventListener("click", pauseString);
+const nextBtn = document
+	.querySelector(".resetBtn")
+	.addEventListener("click", resetString);
+let typedAnim;
+let pauseStart = true;
 function animationTypedJS() {
-	let typedAnim = new Typed(".a-proposTxt", {
+	typedAnim = new Typed(".a-proposTxt", {
 		strings: [
 			`<p >Je suis actuellement en formation de développeur web/web mobile dispensé par l'<a class="linksTyped" href="https://www.afpa.fr/" target="_blank">AFPA </a>. Durant cette formation, j'ai acquis des compétences en front-end avec des technologies telles que HTML, CSS, JS et VUE.js, et en back-end avec des technologies comme PHP et SYMFONY.</p>`,
 			`<p>Mon objectif est de devenir un développeur web compétent et polyvalent, capable de concevoir des applications web de haute qualité pour répondre aux besoins des utilisateurs et des clients. Si vous êtes à la recherche d'un développeur passionné et motivé pour votre prochain projet, n'hésitez pas à me <a class="linksTyped" href="#contact">contacter</a>.</p>`,
@@ -8,12 +17,35 @@ function animationTypedJS() {
 		cursorChar: "",
 		typeSpeed: 25,
 		backSpeed: 80,
-		loop: true,
-		backDelay: 10000,
+		loop: false,
+		backDelay: 3000,
 		fadeOut: true,
 	});
-
 	console.log(typedAnim);
 }
 
 animationTypedJS();
+
+function pauseString() {
+	let pauseBtn = document.querySelector(".pauseBtn");
+	if (typedAnim && pauseStart) {
+		typedAnim.stop();
+		pauseBtn.innerHTML = '<i class="fas fa-play"></i>';
+		pauseStart = false;
+	} else {
+		typedAnim.start();
+		pauseBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+		pauseStart = true;
+	}
+}
+
+function resetString() {
+	let pauseBtn = document.querySelector(".pauseBtn");
+	if (typedAnim && pauseStart) {
+		typedAnim.reset();
+	} else {
+		pauseBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+		typedAnim.reset();
+		pauseStart = false;
+	}
+}
--- Js/position.js
+++ Js/position.js
@@ -6,6 +6,10 @@ function getFooterPosition() {
 	return document.querySelector("footer").getBoundingClientRect();
 }
 
+function getPopupButtonHeight() {
+	return document.querySelector("#ouvrir-popup").offsetHeight;
+}
+
 function isFooterVisible() {
 	const footerPosition = getFooterPosition();
 	return footerPosition.top < window.innerHeight && footerPosition.bottom >= 0;
@@ -13,11 +17,17 @@ function isFooterVisible() {
 
 function updatePopupButtonPosition() {
 	const popupButton = document.querySelector("#ouvrir-popup");
+	const popupContainer = document.querySelector(".popup-container");
 	if (isFooterVisible()) {
+		popupContainer.style.bottom = `${
+			getFooterHeight() + getPopupButtonHeight()
+		}px`;
 		popupButton.style.bottom = `${getFooterHeight()}px`;
 		console.log("replacement du bouton météo");
+		console.log(getPopupButtonHeight());
 	} else {
 		popupButton.style.bottom = "0";
+		popupContainer.style.bottom = `70px`;
 	}
 }
 
